7b7271603fa32d2e7b31712fb6bd88ccb274a8c3
Ragnar <ragnar.naruski@voco.ee>
2023-09-15 17:24:26 +0300

    week2

--- src/week2/Ex32.java
+++ src/week2/Ex32.java
@@ -1,4 +1,20 @@
 package week2;
 
+import java.util.Scanner;
+
 public class Ex32 {
+    public static void main(String[] args) {
+        System.out.println("Until what number?");
+        Scanner reader = new Scanner(System.in);
+        int num = Integer.parseInt(reader.nextLine());
+        int sum = 0;
+        int add = 1;
+        int i = 0;
+        while (i < num) {
+            sum=sum+ add;
+            add++;
+            i++;
+        }
+        System.out.println("Sum is:" +sum);
+    }
 }
